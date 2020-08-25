2ef9b87cd63015b664b97d08340e785912247556
Matthew Berry <me@mattrb.com>
2020-08-24 23:21:52 -0700

    check condition before executing instruction

--- src/crab/cpu.cr
+++ src/crab/cpu.cr
@@ -1,5 +1,6 @@
 class CPU
   @r = Slice(Word).new 16
+  @cpsr : UInt32 = 0
   @pipeline = Deque(Word).new 2
 
   def initialize(@gba : GBA)
@@ -8,14 +9,36 @@ class CPU
 
   def fill_pipeline : Nil
     while @pipeline.size < 2
-      puts "FETCH PC: #{hex_str @r[15]}, INSTR: #{hex_str @gba.bus.read_word @r[15]}, TYPE: #{Instr.from_hash hash_instr @gba.bus.read_word @r[15]}"
+      puts "Fetch pc: #{hex_str @r[15]}, instr: #{hex_str @gba.bus.read_word @r[15]}, type: #{Instr.from_hash hash_instr @gba.bus.read_word @r[15]}"
       @pipeline << @gba.bus.read_word @r[15]
       @r[15] &+= 4
     end
   end
 
   def clear_pipeline : Nil
+    puts "Clearing pipeline"
     @pipeline.clear
+  end
+
+  def check_cond(cond : Int) : Bool
+    case 0xF_u8 & cond & 0xF
+    when 0x0 then bit? @cpsr, 30                                            # Z
+    when 0x1 then !bit? @cpsr, 30                                           # !Z
+    when 0x2 then bit? @cpsr, 29                                            # C
+    when 0x3 then !bit? @cpsr, 29                                           # !C
+    when 0x4 then bit? @cpsr, 31                                            # N
+    when 0x5 then !bit? @cpsr, 31                                           # !N
+    when 0x6 then bit? @cpsr, 28                                            # V
+    when 0x7 then !bit? @cpsr, 28                                           # !V
+    when 0x8 then (bit? @cpsr, 29) && (!bit? @cpsr, 30)                     # C && !Z
+    when 0x9 then (!bit? @cpsr, 29) || (bit? @cpsr, 30)                     # !C || Z
+    when 0xA then (bit? @cpsr, 31) == (bit? @cpsr, 28)                      # N == V
+    when 0xB then (bit? @cpsr, 31) != (bit? @cpsr, 28)                      # N != V
+    when 0xC then (!bit? @cpsr, 30) && (bit? @cpsr, 31) == (bit? @cpsr, 28) # !Z && N == V
+    when 0xD then (bit? @cpsr, 30) || (bit? @cpsr, 31) != (bit? @cpsr, 28)  # Z || N != V
+    when 0xE then true                                                      # always
+    else          raise "Cond 0xF is reserved"                              # reserved
+    end
   end
 
   def tick : Nil
@@ -29,22 +52,23 @@ class CPU
   end
 
   def execute(instr : Word) : Nil
-    cond = true
-    if cond
+    if check_cond instr >> 28
       hash = hash_instr instr
       instr_type = Instr.from_hash hash
+      puts "Execute #{hex_str instr}: #{instr_type}"
       case instr_type
       when Instr::BRANCH
-        puts "EXECUTE BRANCH"
         link = bit? instr, 24
         offset = instr & 0xFFFFFF
         offset = (~offset &+ 1).to_i32 if bit? offset, 23 # negative
         offset <<= 2
         @r[14] = @r[15] - 4 if link
         @r[15] &+= offset
-        @pipeline.clear
+        clear_pipeline
       else raise "Unimplemented execution of type: #{instr_type}"
       end
+    else
+      puts "Skipping instruction, cond: #{hex_str instr >> 28}"
     end
   end
 end
