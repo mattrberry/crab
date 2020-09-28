7820e69eb632ff9ab6bc475c4b04c67838f238dd
Matthew Berry <me@mattrb.com>
2020-09-27 22:41:54 -0700

    thumb actually check the condition...

--- src/crab/thumb/conditional_branch.cr
+++ src/crab/thumb/conditional_branch.cr
@@ -2,7 +2,7 @@ module THUMB
   def thumb_conditional_branch(instr : Word) : Nil
     cond = bits(instr, 8..11)
     offset = bits(instr, 0..7).to_i8!
-    if cond
+    if check_cond cond
       @r[15] &+= (offset * 2)
       clear_pipeline
     end
