71235e4b2a1d4015557818957bff75d7df5e711d
Matthew Berry <me@mattrb.com>
2020-10-09 20:17:51 -0700

    arm fix negative branch offset

--- src/crab/arm/branch.cr
+++ src/crab/arm/branch.cr
@@ -2,8 +2,7 @@ module ARM
   def arm_branch(instr : Word) : Nil
     link = bit?(instr, 24)
     offset = instr & 0xFFFFFF
-    offset = (~offset &+ 1).to_i32 if bit?(offset, 23) # negative
-    offset <<= 2
+    offset = (offset << 8).to_i32! >> 6
     @r[14] = @r[15] - 4 if link
     @r[15] &+= offset
     clear_pipeline
