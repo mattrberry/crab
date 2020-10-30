c8e84d8bed1ee95bd9fd04153cd33f5872295e31
Matthew Berry <me@mattrb.com>
2020-10-30 00:30:13 -0700

    thumb load address bit 1 of pc should always be 0

--- src/crab/thumb/load_address.cr
+++ src/crab/thumb/load_address.cr
@@ -4,6 +4,7 @@ module THUMB
     rd = bits(instr, 8..10)
     word = bits(instr, 0..8)
     imm = word << 2
-    set_reg(rd, (source ? @r[13] : @r[15]) &+ imm)
+    # Where the PC is used as the source register (SP = 0), bit 1 of the PC is always read as 0.
+    set_reg(rd, (source ? @r[13] : @r[15] & ~2) &+ imm)
   end
 end
