ba6fde31b1ab2671e9ced00e5d849ddbdcf4fd00
Matthew Berry <me@mattrb.com>
2020-11-02 00:14:15 -0800

    arm block data transfer tune up, PASSING ARMWRESTLER

--- src/crab/arm/block_data_transfer.cr
+++ src/crab/arm/block_data_transfer.cr
@@ -2,32 +2,46 @@ module ARM
   def arm_block_data_transfer(instr : Word) : Nil
     pre_index = bit?(instr, 24)
     add = bit?(instr, 23)
-    s_bit = bit?(instr, 22) # todo respect this bit
+    s_bit = bit?(instr, 22)
     write_back = bit?(instr, 21)
     load = bit?(instr, 20)
     rn = bits(instr, 16..19)
     list = bits(instr, 0..15)
 
+    if s_bit
+      abort "todo: handle cases with r15 in list" if bit?(list, 15)
+      mode = @cpsr.mode
+      switch_mode CPU::Mode::USR
+    end
+
     address = @r[rn]
 
+    step = add ? 4 : -4
+    idxs = add ? 16.times : 15.downto(0)
+
     if load
-      16.times do |idx|
+      idxs.each do |idx|
         if bit?(list, idx)
-          address &+= add ? 4 : -4 if pre_index
+          address &+= step if pre_index
           set_reg(idx, @gba.bus.read_word(address))
-          address &+= add ? 4 : -4 unless pre_index
+          address &+= step unless pre_index
         end
       end
     else
-      15.downto(0).each do |idx|
+      idxs.each do |idx|
         if bit?(list, idx)
-          address &+= add ? 4 : -4 if pre_index
+          address &+= step if pre_index
           @gba.bus[address] = @r[idx]
-          address &+= add ? 4 : -4 unless pre_index
+          address &+= step unless pre_index
         end
       end
     end
 
-    set_reg(rn, address) if write_back
+    if s_bit
+      switch_mode CPU::Mode.from_value mode.not_nil!
+    end
+
+    # todo stm where rn is 2nd or further in list loads the original address
+    set_reg(rn, address) if write_back && !(load && bit?(list, rn))
   end
 end
