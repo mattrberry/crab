768457178f965686bde1bbc3dd4c7da2fb175f00
Matthew Berry <me@mattrb.com>
2020-11-22 12:02:10 -0800

    psr transfer - properly generate mask (gbatek is right, arm manual is wrong)

--- src/crab/arm/psr_transfer.cr
+++ src/crab/arm/psr_transfer.cr
@@ -1,31 +1,28 @@
 module ARM
   def arm_psr_transfer(instr : Word) : Nil
     spsr = bit?(instr, 22)
-    msr = bit?(instr, 21)
-    if msr
-      all = bit?(instr, 16)
-      if all && CPU::Mode.from_value(@cpsr.mode) != CPU::Mode::USR
-        mask = 0x00000000_u32
-      else
-        mask = 0x0FFFFFFF_u32
-      end
-      imm_flag = bit?(instr, 25)
-      if imm_flag
+    if bit?(instr, 21) # MSR
+      mask = 0_u32 # described in gbatek, not in arm manual https://problemkaputt.de/gbatek.htm#armopcodespsrtransfermrsmsr
+      mask |= 0xFF000000 if bit?(instr, 19) # f (aka _flg)
+      mask |= 0x00FF0000 if bit?(instr, 18) # s
+      mask |= 0x0000FF00 if bit?(instr, 17) # x
+      mask |= 0x000000FF if bit?(instr, 16) # c (aka _ctl)
+      if bit?(instr, 25) # immediate
         barrel_shifter_carry_out = false # unused, doesn't matter
         value = immediate_offset bits(instr, 0..11), pointerof(barrel_shifter_carry_out)
-      else
-        rm = bits(instr, 0..3)
-        value = @r[rm]
+      else # register value
+        value = @r[bits(instr, 0..3)]
       end
+      value &= mask
       if spsr
-        @spsr.value = (@spsr.value & mask) | (value & ~mask)
+        @spsr.value = (@spsr.value & ~mask) | value
       else
         thumb = @cpsr.thumb
-        switch_mode CPU::Mode.from_value value & 0x1F if all && CPU::Mode.from_value(@cpsr.mode) != CPU::Mode::USR
-        @cpsr.value = (@cpsr.value & mask) | (value & ~mask)
+        switch_mode CPU::Mode.from_value value & 0x1F if mask & 0xFF > 0
+        @cpsr.value = (@cpsr.value & ~mask) | value
         @cpsr.thumb = thumb
       end
-    else
+    else # MRS
       rd = bits(instr, 12..15)
       if spsr
         set_reg(rd, @spsr.value)
