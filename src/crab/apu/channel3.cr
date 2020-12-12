9c2d135293c436c38680ff59b6a1c88caf9b3e46
Matthew Berry <me@mattrb.com>
2020-12-11 23:41:44 -0800

    updated channel3 from cgb audio to gba audio

--- src/crab/apu/channel3.cr
+++ src/crab/apu/channel3.cr
@@ -6,24 +6,30 @@ class Channel3 < SoundChannel
     value.is_a?(Int) && RANGE.includes?(value) || WAVE_RAM_RANGE.includes?(value)
   end
 
-  @wave_ram = Bytes.new(WAVE_RAM_RANGE.size) { |idx| idx & 1 == 0 ? 0x00_u8 : 0xFF_u8 }
+  @wave_ram = Array(Bytes).new 2, Bytes.new(WAVE_RAM_RANGE.size) { |idx| idx & 1 == 0 ? 0x00_u8 : 0xFF_u8 }
   @wave_ram_position : UInt8 = 0
   @wave_ram_sample_buffer : UInt8 = 0x00
+
+  # NR30
+  @wave_ram_dimension : Bool = false
+  @wave_ram_bank : UInt8 = 0
 
   # NR31
   @length_load : UInt8 = 0x00
 
   # NR32
   @volume_code : UInt8 = 0x00
+  @volume_force : Bool = false
 
-  @volume_code_shift : UInt8 = 0
+  @volume_multiplier : Float32 = 0
 
   # NR33 / NR34
   @frequency : UInt16 = 0x00
 
   def step_wave_generation : Nil
     @wave_ram_position = (@wave_ram_position + 1) % (WAVE_RAM_RANGE.size * 2)
-    @wave_ram_sample_buffer = @wave_ram[@wave_ram_position // 2]
+    @wave_ram_bank ^= 1 if @wave_ram_position == 0 && @wave_ram_dimension
+    @wave_ram_sample_buffer = @wave_ram[@wave_ram_bank][@wave_ram_position // 2]
   end
 
   def frequency_timer : UInt32
@@ -36,7 +42,7 @@ class Channel3 < SoundChannel
 
   def get_amplitude : Float32
     if @enabled && @dac_enabled
-      dac_input = ((@wave_ram_sample_buffer >> (@wave_ram_position & 1 == 0 ? 4 : 0)) & 0x0F) >> @volume_code_shift
+      dac_input = @volume_multiplier * ((@wave_ram_sample_buffer >> (@wave_ram_position & 1 == 0 ? 4 : 0)) & 0x0F)
       dac_output = (dac_input / 7.5) - 1
       dac_output
     else
@@ -53,9 +59,9 @@ class Channel3 < SoundChannel
     when 0x75 then 0xBF | (@length_enable ? 0x40 : 0)
     when WAVE_RAM_RANGE
       if @enabled
-        @wave_ram[@wave_ram_position // 2]
+        @wave_ram[@wave_ram_bank][@wave_ram_position // 2]
       else
-        @wave_ram[index - WAVE_RAM_RANGE.begin]
+        @wave_ram[@wave_ram_bank][index - WAVE_RAM_RANGE.begin]
       end
     else raise "Reading from invalid Channel3 register: #{hex_str index.to_u16}"
     end.to_u8
@@ -66,20 +72,24 @@ class Channel3 < SoundChannel
     when 0x70
       @dac_enabled = value & 0x80 > 0
       @enabled = false if !@dac_enabled
+      @wave_ram_dimension = bit?(value, 5)
+      @wave_ram_bank = bits(value, 6..6)
     when 0x72
       @length_load = value
       # Internal values
       @length_counter = 0x100 - @length_load
     when 0x73
       @volume_code = (value & 0x60) >> 5
+      @volume_force = bit?(value, 7)
       # Internal values
-      @volume_code_shift = case @volume_code
-                           when 0b00 then 4
-                           when 0b01 then 0
-                           when 0b10 then 1
-                           when 0b11 then 2
-                           else           raise "Impossible volume code #{@volume_code}"
-                           end.to_u8
+      @volume_multiplier = case {@volume_force, @volume_code}
+                           when {true, _} then 0.75_f32
+                           when {_, 0b00} then 0_f32
+                           when {_, 0b01} then 1_f32
+                           when {_, 0b10} then 0.5_f32
+                           when {_, 0b11} then 0.25_f32
+                           else                raise "Impossible volume code #{@volume_code}"
+                           end
     when 0x74
       @frequency = (@frequency & 0x0700) | value
     when 0x75
@@ -113,9 +123,9 @@ class Channel3 < SoundChannel
       end
     when WAVE_RAM_RANGE
       if @enabled
-        @wave_ram[@wave_ram_position // 2] = value
+        @wave_ram[@wave_ram_bank][@wave_ram_position // 2] = value
       else
-        @wave_ram[index - WAVE_RAM_RANGE.begin] = value
+        @wave_ram[@wave_ram_bank][index - WAVE_RAM_RANGE.begin] = value
       end
     else raise "Writing to invalid Channel3 register: #{hex_str index.to_u16}"
     end
