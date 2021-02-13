96723e606b9b8cfe04160585f1fff54904ae5845
wheremyfoodat <gponiris2004@gmail.com>
2021-02-13 10:54:27 -0800

    Minor nitpick

--- src/crab/arm/branch_exchange.cr
+++ src/crab/arm/branch_exchange.cr
@@ -1,11 +1,7 @@
 module ARM
   def arm_branch_exchange(instr : Word) : Nil
     rn = bits(instr, 0..3)
-    if bit?(@r[rn], 0)
-      @cpsr.thumb = true
-      set_reg(15, @r[rn])
-    else
-      set_reg(15, @r[rn])
-    end
+    @cpsr.thumb = bit?(@r[rn], 0) 
+    set_reg(15, @r[rn])
   end
 end
