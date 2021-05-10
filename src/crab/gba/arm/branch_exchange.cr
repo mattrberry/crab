8b6f300f671312c3d8d2c9f0df39da4e75004f69
Matthew Berry <me@mattrb.com>
2021-05-09 23:48:23 -0700

    simplify arm branch exchange logic

--- src/crab/gba/arm/branch_exchange.cr
+++ src/crab/gba/arm/branch_exchange.cr
@@ -1,13 +1,9 @@
 module GBA
   module ARM
     def arm_branch_exchange(instr : Word) : Nil
-      rn = bits(instr, 0..3)
-      if bit?(@r[rn], 0)
-        @cpsr.thumb = true
-        set_reg(15, @r[rn])
-      else
-        set_reg(15, @r[rn])
-      end
+      address = @r[bits(instr, 0..3)]
+      @cpsr.thumb = bit?(address, 0)
+      set_reg(15, address)
     end
   end
 end
